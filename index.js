7f7de731b05bfdf8a969567888bccf3842a76dd5
MimiCode21 <brown.mimi2458@gmail.com>
2023-07-21 15:16:38 +0100

    edited the popover buttons and enabled the use of html code in the content

--- index.js
+++ index.js
@@ -14,5 +14,29 @@ navbarToggle.addEventListener("click", () => {
 
 // popovers
 
+// <!-- Initialize the popover in JavaScript -->
+{/* <script> */}
+  $(document).ready(function() {
+    $('[data-toggle="popover"]').popover({
+      trigger: 'click', // Show the popover on hover
+      // Add any other options you want to customize the popover behavior
+      
+    });
+  });
+// </script>
+
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+
+// {/* <script> */}
+// $(document).ready(function(){
+//   $('[data-toggle="popover"]').popover();   
+// });
+// {/* </script> */}
+
+
+
+// $(document).ready(function(){
+//     $('.btn').popover({title: "<h1><strong>HTML</strong> inside <code>the</code> <em>popover</em></h1>", content: "Blabla <br> <h2>Cool stuff!</h2>", html: true, placement: "right"}); 
+//   });
